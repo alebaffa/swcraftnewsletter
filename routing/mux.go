7e7d14ebb89d07946a0cc8c5daad834595fc482b
alebaffa <alessandro.baffa@gmail.com>
2016-08-15 21:49:02 +0200

    split router in another package

--- routing/mux.go
+++ routing/mux.go
@@ -3,6 +3,8 @@ package routing
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func HomePageHandler(res http.ResponseWriter, req *http.Request) {
@@ -15,4 +17,12 @@ func executeTemplate(templateType string, res http.ResponseWriter) {
 	if err := templates.ExecuteTemplate(res, templateType, nil); err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func NewMux() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomePageHandler)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./assets/")))
+	http.Handle("/", r)
+	return r
 }
--- server.go
+++ server.go
@@ -4,13 +4,8 @@ import (
 	"net/http"
 
 	"github.com/alebaffa/newsletter-web/routing"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", routing.HomePageHandler)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./assets/")))
-	http.Handle("/", r)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", routing.NewMux())
 }
