7c06cad6f004e402f9bdd61b05331e0ac1345ac9
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-10-16 10:43:27 +0200

    remove private

--- private/private.go
+++
@@ -1,9 +0,0 @@
-package private
-
-func GetParams() (string, string, string, string) {
-	email := "alessandro.baffa@gmail.com"
-	password := "aprqyqwbbwoibtuc"
-	smtpServer := "smtp.gmail.com"
-	port := "587"
-	return email, password, smtpServer, port
-}
