ccb56bafd9f46f83357d9e5c0cf576f4bd2875c4
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-10-16 10:05:01 +0200

    update gitignore

---
+++ private/private.go
@@ -0,0 +1,9 @@
+package private
+
+func GetParams() (string, string, string, string) {
+	email := "alessandro.baffa@gmail.com"
+	password := "aprqyqwbbwoibtuc"
+	smtpServer := "smtp.gmail.com"
+	port := "587"
+	return email, password, smtpServer, port
+}
