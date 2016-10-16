e4d3ee92a89ceb0cc2bbe41421bdceece04ce2fb
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-10-16 10:34:25 +0200

    commit all files

--- pushdeploy.sh
+++ pushdeploy.sh
@@ -3,11 +3,12 @@ echo "Commit..."
 git add .
 git commit -m "commit all files"
 
-echo "push to heroku..."
-git push heroku master
-
-echo "replace private..."
+echo "remove private..."
+rm private/private.go
 cp private.go private/
 
 echo "push to github..."
 git push origin master
+
+echo "push to heroku..."
+git push heroku master
