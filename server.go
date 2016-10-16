34b409912208cf4c4a6cd593eb1e1a3d27c98ae9
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-10-16 10:31:19 +0200

    commit all files

--- server.go
+++ server.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"net/http"
-
+	"os"
 	"github.com/alebaffa/swcraftnewsletter/routing"
 )
 
 func main() {
-	//port := os.Getenv("PORT") //heroku
-	port := "8080"
+	port := os.Getenv("PORT") //heroku
+	//port := "8080"
 	http.ListenAndServe(":"+port, routing.NewMux())
 }
