e85961df042997a6f5d50b1337a00a6d248b76d9
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-10-16 19:57:30 +0200

    refactor sender test

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -6,31 +6,28 @@ import (
 )
 
 func TestEmail_SendSuccessful(t *testing.T) {
-	f, r := mockSend(nil)
-	sender := &emailSender{send: f}
+	function, emailRecorder := mockSend()
+
+	sender := &emailSender{send: function}
 	body := "Hello World"
 	err := sender.SendMail([]string{"me@example.com"}, []byte(body))
 
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
-	if string(r.msg) != body {
-		t.Errorf("wrong message body.\n\nexpected: %\n got: %s", body, r.msg)
+	if string(emailRecorder.msg) != body {
+		t.Errorf("wrong message body.\n\nexpected: %\n got: %s", body, emailRecorder.msg)
 	}
 }
 
-func mockSend(errToReturn error) (func(string, smtp.Auth, string, []string, []byte) error, *emailRecorder) {
+func mockSend() (func(string, smtp.Auth, string, []string, []byte) error, *emailRecorder) {
 	r := new(emailRecorder)
 	return func(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
-		*r = emailRecorder{addr, a, from, to, msg}
-		return errToReturn
+		*r = emailRecorder{msg}
+		return nil
 	}, r
 }
 
 type emailRecorder struct {
-	addr string
-	auth smtp.Auth
-	from string
-	to   []string
-	msg  []byte
+	msg []byte
 }
